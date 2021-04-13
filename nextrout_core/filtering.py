custom ctrl files add, weighting mets add, bug:flux coming from dmk_cont

nextrout_core/filtering.py
@@ -283,7 +283,10 @@ def filtering(Gpe, sources, sinks,beta_d = 1.5,threshold=1e-3, tdens0 = 1, BPwei
     # tdens0
 
     if tdens0 != 1:
-        tdens0 = np.array([(Gpe_rel.edges[edge]['weight']) for edge in edges])
+        try:
+            tdens0 = np.array([(Gpe_rel.edges[edge]['tdens']) for edge in edges])
+        except:
+            tdens0 = np.array([(Gpe_rel.edges[edge]['flux']) for edge in edges])
 
     # topol
 
@@ -324,7 +327,7 @@ def filtering(Gpe, sources, sinks,beta_d = 1.5,threshold=1e-3, tdens0 = 1, BPwei
 
     # init and set controls
     ctrl = Dmkcontrols.DmkCtrl()
-    Dmkcontrols.get_from_file(ctrl,'../dmk_utilities/dmk_solver/graph_otp_solver/python/examples/FaccaBenzi2021_TestCase1/dmk.ctrl')
+    Dmkcontrols.get_from_file(ctrl,'./dmk_discr.ctrl')
     # if and where save data
     ctrl.id_save_dat=1
     ctrl.fn_tdens='tdens.dat'
@@ -359,28 +362,32 @@ def filtering(Gpe, sources, sinks,beta_d = 1.5,threshold=1e-3, tdens0 = 1, BPwei
             if abs(flux[ed_count])> max_flux*threshold:
                 Gf.add_edge(*edge,  flux = flux[ed_count])
                 weights_in_Gf.append(flux[ed_count])
+
         elif BPweights == 'tdens':
             if abs(tdens[ed_count])> max_tdens*threshold:
                 Gf.add_edge(*edge,  tdens = tdens[ed_count])
                 weights_in_Gf.append(tdens[ed_count])
+
         else:
             raise ValueError('BPweights flag not defined!.')
+        Gf.add_node(edge[0], weight = Gpe_rel.nodes[edge[0]]['tdens'])# todo: this needs to be fixed once the flux is working again (BPweights)
+        Gf.add_node(edge[1], weight = Gpe_rel.nodes[edge[1]]['tdens'])
 
 
+    Gf.remove_nodes_from(list(nx.isolates(Gf)))
+        
     weights_in_Gf = np.array(weights_in_Gf)
     colors = []
 
     for node in Gf.nodes():
+
         Gf.nodes[node]['pos'] = Gpe_rel.nodes[node]['pos']
+
         if node in sources_rel:
             colors.append('g')
         elif node in sinks_rel:
             colors.append('r')
         else:
             colors.append('k')
-
-    # reweighing simplification
-
-    ### pending!
 
     return Gf, weights_in_Gf, colors

nextrout_core/main.py
@@ -19,10 +19,12 @@ def nextrout(
     ndiv=15, 
     graph_type='1',
     weighting_method = 'ER',
+    DMKw = 'tdens',
     min_pe = 0.01, 
     min_f = 0.001,
     verbose = True, 
-    BPw = 'flux', 
+    weighting_method_simplification = 'ER',
+    BPw = 'tdens0', 
     stop_thresh_f = 1e-6, 
     btns_factor_source=.1, 
     btns_factor_sink=.1,
@@ -48,16 +50,16 @@ def nextrout(
 
     # run the graph extraction
 
-    Gpe = pre_extraction.pre_extr(coord, topol, tdpot, min_= min_pe, graph_type=graph_type,weighting_method = weighting_method)
+    Gpe = pre_extraction.pre_extr(coord, topol, tdpot, min_= min_pe, graph_type=graph_type,weighting_method = weighting_method, DMKw = DMKw)
 
     if storing is not None:
-        weights = np.array([Gpe.edges[edge]['weight'] for edge in Gpe.edges()])
+        weights = np.array([Gpe.edges[edge][DMKw] for edge in Gpe.edges()])
         max_w = max(weights)
         weights/=max_w
         fig1, ax1 = plt.subplots(figsize=(10, 10))
         ax1.tricontour(triang, forcing, levels=40, linewidths=0.1, colors='k')
         pos = nx.get_node_attributes(Gpe,'pos')
-        nx.draw(Gpe,pos, node_size = 10, node_color = 'k',width = weights, ax = ax1)
+        nx.draw(Gpe,pos, node_size = 10, node_color = 'k',width = weights*3, ax = ax1)
         plt.savefig(storing+'/Gpe.png')
         plt.close()
 
@@ -81,6 +83,9 @@ def nextrout(
         temp_sources = [node for node in sources if node in cc]
         temp_sinks = [node for node in sinks if node in cc]
 
+        if len(temp_sources) ==0 or len(temp_sinks) == 0:
+            raise ValueError('Not enough sources or sinks.')
+
         temp_Gf,weights,colors = filtering.filtering(
             temp_Gpe, 
             temp_sources, 
@@ -103,18 +108,51 @@ def nextrout(
             plt.savefig(storing+'/Gf_'+str(count)+'.png')
             plt.close()
 
+    
+
+    deg = nx.degree_centrality(Gf)
+
+    if weighting_method_simplification == 'ER':
+
+        N = len(Gf.nodes())
+
+        for edge in Gf.edges():
+
+            Gf.edges[(edge[0], edge[1])][BPw] = Gf.nodes[edge[0]][
+                                                                 'weight'] / (
+                                                                     deg[edge[0]] * (N - 1)) + \
+                                                             Gf.nodes[edge[1]][
+                                                                 'weight'] / (
+                                                                     deg[edge[1]] * (N - 1))
+    elif weighting_method_simplification == 'IBP':
+        
+        raise ValueError('not implemented yet.')
+        '''
+        Gf_relabeled = relabeling(Gf, Gpe)
+        for edge in Gf_relabeled.edges():
+            Gf.edges[edge[0],edge[1]][BPw]=Gpe.edges[edge[0],edge[1]]['weight']
+        '''
+        
+    elif weighting_method_simplification == 'BPW':
+        pass
+
+
     if storing is not None:
         if len(cc_list)==1:
             color = colors
         else:
             color = 'k'
         weights = np.array([Gf.edges[edge][BPw] for edge in Gf.edges()])
+        max_w = max(weights)
+        weights/=max_w
         fig1, ax1 = plt.subplots(figsize=(10, 10))
         ax1.tricontour(triang, forcing, levels=40, linewidths=0.1, colors='k')
         pos = nx.get_node_attributes(Gf,'pos')
         nx.draw(Gf,pos, node_size = 30, node_color = color, width = abs(weights)*3, ax = ax1 )
         plt.savefig(storing+'/Gf.png')
         plt.close()
+
+    
     # storing the results
 
     if storing is not None:
@@ -124,6 +162,8 @@ def nextrout(
             with open(storing + '/'+ff[0]+'.pkl', 'wb') as file:
                 pkl.dump(ff[1], file)
 
+    #print('isolated nodes?:',len(list(nx.isolates(Gf))))
+
     return Gf
 
 
@@ -131,7 +171,7 @@ def nextrout(
 
 # generate forcing
 
-forcing_flag = 'rect_cnst_d'
+forcing_flag = 'rect_cnst'
 
 if forcing_flag == 'rect_cnst':
 
@@ -232,7 +272,7 @@ elif forcing_flag == 'dirac3':
                     'xplus':xplus,
                     'xminus':xminus}
 
-beta_c = 1.3
+beta_c = 1.5
 beta_d = 1.5
 flags = ['whole_convex_hull+btns_centr','branch_convex_hull+btns_centr','btns_centr','single']
 
@@ -242,16 +282,17 @@ nextrout(forcing_flag,
     extra_info,
     beta_c,
     beta_d = beta_d, 
-    ndiv = 18, 
-    graph_type='3',
-    weighting_method = 'AVG',
-    min_pe = 0.001,
-    min_f = 0.32,
+    ndiv = 30, 
+    graph_type='1',
+    weighting_method = 'ER',
+    min_pe = 0.01,
+    min_f = 0.1,
     BPw = 'flux',
+    weighting_method_simplification ='ER',
     stop_thresh_f = 1e-8,
     verbose = False,
     weight_flag = 'length',
     btns_factor_source=.5, 
     btns_factor_sink=.5,
-    terminal_criterion =  flags[2],
+    terminal_criterion =  flags[3],
     storing = './outputs/')

nextrout_core/pre_extraction.py
@@ -4,7 +4,7 @@ import numpy as np
 from itertools import combinations
 
 
-def dmk2pre_extr_inputs(coord, topol, tdpot):
+def dmk2pre_extr_inputs(coord, topol, tdpot, DMKw = 'tdens'):
 	coordinates = {}
 	print('len coord 1', len(coord))
 	k=-1
@@ -24,7 +24,12 @@ def dmk2pre_extr_inputs(coord, topol, tdpot):
 	  coord_of_nodes_in_T = np.array([coordinates[node] for node in T])
 	  center = sum(coord_of_nodes_in_T)/3
 	  centers[k] = center
-	  G_bar.add_node(k,pos = center, weight = tdpot.tdens[k])
+	  if DMKw == 'tdens':
+	  	G_bar.add_node(k,pos = center, tdens = tdpot.tdens[k])
+	  elif DMKw == 'flux':
+	  	G_bar.add_node(k,pos = center, flux = abs(tdpot.flux[k]))
+	  else:
+	  	raise ValueError('DMKw not defined.')
 
 	for node in G_triang.nodes():
 	  G_triang.nodes[node]['pos'] = coordinates[node]
@@ -203,21 +208,28 @@ def connecting_edges(G_bar, node, min_, graph_type, dict_seq, max_, weighting_me
 			node = node - 1
 			dict_sec_neig, index_ = get_sec_neig_edges_square(node, dict_seq, node2box_index)
 
-
+	# getting weight flag
+	try: 
+		node_test = list(G_bar.nodes())[0]
+		G_bar.nodes[node_test]['tdens']
+		DMKw = 'tdens'
+	except:
+		DMKw = 'flux'
 	#print(index_)
 	for bar_ in index_:
 		if graph_type == "1" or graph_type == "2":  # both edges and vertices
 			# Checking if the weights are greater than the threshold x max
 			if (
-					G_bar.nodes[bar_]["weight"] > min_ * max_
-					and G_bar.nodes[node]["weight"] > min_ * max_
+					G_bar.nodes[bar_][DMKw] > min_ * max_
+					and G_bar.nodes[node][DMKw] > min_ * max_
 			):
 				if weighting_method == "AVG":
 					# If True, then we assign weights to the edges based on the 'AVG' method
-					w_list = [G_bar.nodes[bar_]["weight"], G_bar.nodes[node]["weight"]]
+					w_list = [G_bar.nodes[bar_][DMKw], G_bar.nodes[node][DMKw]]
 					w = sum(w_list) / len(w_list)  # <--- the average
 					# w=w0 #<--- this is the minimum
-					G_bar.add_edge(node, bar_, weight=w)
+					G_bar.add_edge(node, bar_)
+					G_bar.edges[(node, bar_)][DMKw] = w
 				elif weighting_method == "ER":
 					# If True, then we just add the edge. Later on we define the weight.
 					G_bar.add_edge(node, bar_)
@@ -236,8 +248,14 @@ def grid_filter(G_bar, G_triang, min_, dict_seq):
 		 G_pre_extracted: the input graph but with edges generated according to graph_type 3.
 	'''
 
-	nodes_dict = G_bar.nodes(data='weight')
-	max_ = max([entry[1] for entry in nodes_dict])
+	try:
+		nodes_dict = G_bar.nodes(data='tdens')
+		max_ = max([entry[1] for entry in nodes_dict])
+		DMKw = 'tdens'
+	except:
+		nodes_dict = G_bar.nodes(data='flux')
+		max_ = max([entry[1] for entry in nodes_dict])
+		DMKw = 'flux'
 
 	G_pre_extracted = G_triang.copy()
 
@@ -256,12 +274,12 @@ def grid_filter(G_bar, G_triang, min_, dict_seq):
 		# Getting the edges of the triangle for a given bar 'bar_'
 
 		same_triang = get_first_neig(bar_, dict_seq)
-		w = G_bar.nodes[bar_]["weight"]
+		w = G_bar.nodes[bar_][DMKw]
 
 		if w >= min_ * max_:
 			# If True, we add the edges to the graph
 			for node in same_triang:
-				G_pre_extracted.nodes[node]['weight']=0
+				G_pre_extracted.nodes[node][DMKw]=1
 			edges = list(combinations(same_triang, 2))
 			membership = [
 				edges[0] in G_pre_extracted.edges(),
@@ -275,14 +293,15 @@ def grid_filter(G_bar, G_triang, min_, dict_seq):
 				# If the edge is in the graph (thanks to another barycenter), we sum the weight of the barycenter 'bar_' to the existing weight
 				if bool_val == True:
 					new_weight = (
-							G_pre_extracted.edges[(edges[i][0], edges[i][1])]["weight"]
+							G_pre_extracted.edges[(edges[i][0], edges[i][1])][DMKw]
 							+ float(w) / 2.0
 					)
-					G_pre_extracted.edges[(edges[i][0], edges[i][1])]["weight"] = new_weight
+					G_pre_extracted.edges[(edges[i][0], edges[i][1])][DMKw] = new_weight
 				# If the edge is not in the graph, we add half of the weight of the barycenter
 				else:
 					new_weight = float(w) / 2.0
-					G_pre_extracted.add_edge(edges[i][0], edges[i][1], weight=new_weight)
+					G_pre_extracted.add_edge(edges[i][0], edges[i][1])
+					G_pre_extracted.edges[(edges[i][0], edges[i][1])][DMKw] = new_weight
 
 	return G_pre_extracted
 
@@ -304,10 +323,16 @@ def node_edge_filter(G_bar, min_, graph_type, dict_seq, weighting_method,input_f
 	"""
 	This script generates graph type 1 and 2.
 	"""
-	nodes_dict = G_bar.nodes(data='weight')
-	max_ = max([entry[1] for entry in nodes_dict])
+	try:
+		nodes_dict = G_bar.nodes(data='tdens')
+		max_ = max([entry[1] for entry in nodes_dict])
+		DMKw = 'tdens'
+	except:
+		nodes_dict = G_bar.nodes(data='flux')
+		max_ = max([entry[1] for entry in nodes_dict])
+		DMKw = 'flux'
 
-	reduced_node_list = [int(node) for node in G_bar.nodes() if G_bar.nodes[node]['weight']>min_ * max_]
+	reduced_node_list = [int(node) for node in G_bar.nodes() if G_bar.nodes[node][DMKw]>min_ * max_]
 
 	# Iterate over all the numbers (<--> nodes) to test the condition about the threshold:
 	for n in reduced_node_list:#range(len(G_bar.nodes()))
@@ -325,9 +350,9 @@ def node_edge_filter(G_bar, min_, graph_type, dict_seq, weighting_method,input_f
 		# If True, then apply 'ER' method
 
 		for edge in G_bar.edges():
-			G_bar.edges[(edge[0], edge[1])]["weight"] = G_bar.nodes[edge[0]]["weight"] / (
+			G_bar.edges[(edge[0], edge[1])][DMKw] = G_bar.nodes[edge[0]][DMKw] / (
 					deg[edge[0]] * (N - 1)
-			) + G_bar.nodes[edge[1]]["weight"] / (deg[edge[1]] * (N - 1))
+			) + G_bar.nodes[edge[1]][DMKw] / (deg[edge[1]] * (N - 1))
 	elif weighting_method == "AVG":
 		pass
 	else:
@@ -383,9 +408,9 @@ def graph_builder(
 
 		return G_pre_extracted
 
-def pre_extr(coord, topol, tdpot, min_, graph_type='1',weighting_method = 'ER'):
+def pre_extr(coord, topol, tdpot, min_, graph_type='1', DMKw = 'tdens', weighting_method = 'ER'):
 
-	G_bar,G_triang,dict_seq = dmk2pre_extr_inputs(coord, topol, tdpot)
+	G_bar,G_triang,dict_seq = dmk2pre_extr_inputs(coord, topol, tdpot, DMKw = DMKw)
 
 	Gpe = graph_builder(G_bar,G_triang,dict_seq, min_,graph_type=graph_type,weighting_method=weighting_method) 
 